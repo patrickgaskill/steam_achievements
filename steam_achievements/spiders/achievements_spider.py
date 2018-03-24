f98d430bdeed0c9659887bfbdf9cda4ad8e6c527
Patrick Gaskill <patrick.gaskill@gmail.com>
2018-03-24 15:57:34 -0500

    Remove f-strings for scrapy cloud compatibility

--- steam_achievements/spiders/achievements_spider.py
+++ steam_achievements/spiders/achievements_spider.py
@@ -3,8 +3,9 @@ import json
 from steam_achievements.items import App, AchievementPercentage
 
 STEAM_API = "http://api.steampowered.com"
-GET_APP_LIST_URL = f"{STEAM_API}/ISteamApps/GetAppList/v0002/"
-GET_GLOBAL_ACHIEVEMENT_PERCENTAGES_FOR_APP_URL = f"{STEAM_API}/ISteamUserStats/GetGlobalAchievementPercentagesForApp/v0002/"
+GET_APP_LIST_URL = "{}/ISteamApps/GetAppList/v0002/".format(STEAM_API)
+GET_GLOBAL_ACHIEVEMENT_PERCENTAGES_FOR_APP_URL = "{}/ISteamUserStats/GetGlobalAchievementPercentagesForApp/v0002/".format(
+    STEAM_API)
 
 
 class AchievementsSpider(scrapy.Spider):
@@ -17,7 +18,8 @@ class AchievementsSpider(scrapy.Spider):
         for app in apps:
             yield App(app)
             appid = app['appid']
-            percentages_url = f"{GET_GLOBAL_ACHIEVEMENT_PERCENTAGES_FOR_APP_URL}?gameid={appid}"
+            percentages_url = "{url}?gameid={appid}".format(
+                url=GET_GLOBAL_ACHIEVEMENT_PERCENTAGES_FOR_APP_URL, appid=appid)
             yield scrapy.Request(percentages_url, callback=self.achievement_percentages_parse, meta={'appid': appid})
 
     def achievement_percentages_parse(self, response):
